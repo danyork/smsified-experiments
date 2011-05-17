67a8850ccd12c8f5727c3d35fc49ddb69a1480ef
Dan York <dyork@Lodestar2.com>
2011-05-17 16:18:48 -0400

    Updated smsified-send.py to use 'dandemo' account

--- smsified-send.py
+++ smsified-send.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+
 # Really simple python app for playing with sending SMS messages
 # via SMSified - http://www.smsified.com/
+# Created by Dan York - May 2011
 
 import json
 import urllib
 
-senderid = "danyorksmsified"   #SMSified account
+senderid = "dandemo"   #SMSified account
 password = "notmyrealpassword" #SMSified password
-sendernum = "7177455086"       #SMSified phone number
+sendernum = "5853260800"       #SMSified phone number
 
 apiurl = "https://"+senderid+":"+password+"@api.smsified.com/v1/smsmessaging/outbound/"+sendernum+"/requests"
 
